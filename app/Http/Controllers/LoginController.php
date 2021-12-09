2579f0cb1bc21be878b6a5de081f56fe949cfcbf
RizkiAnugrah31 <@kikividian.com>
2021-12-09 17:08:01 +0700

    baru

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,26 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
 
 class LoginController extends Controller
 {
     //
 
-    public function authenticate(Request $request)
-    {
-        $credentials = $request->validate([
+         public function authenticate(Request $request)
+        {
+             $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
-        ]);
+            ]);
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended('dashboard');
         }
 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
-    }
-}
+
+        public function loginForm(Request $request)
+        {
+            $client = new \GuzzleHttp\Client();;
+            $request = $client->get('http://localhost:8001/LoginController/get/'.$request);
+            $response = $request->getBody()->getContents();
+            
+            return $data = json_decode($response, true);
+        }
+
+
+        public function doLogin($Request $request)
+        {
+            $client = new Client();
+            $option = [
+                'header'=> [
+                'Accept'=> 'application/json',
+              'Content-Type' => 'application/json',
+            ],
+            'json'=> $request->all ()
+        };
+            $responseService= $client->request9('GET', env(''). '/', $option)
+            $response
+            
+
+
+
+
+    
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/', function () {
 });
 
 Route::get('/login','/LoginController@index');
+Route::post('/', '/');
+
 
